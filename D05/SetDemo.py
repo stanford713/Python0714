70f3186b4770761c6b411282075cccb2d3c7289f
stanford <stanford93713@gmail.com>
2020-07-21 21:59:58 +0800

    python

--- D05/SetDemo.py
+++ D05/SetDemo.py
@@ -4,8 +4,10 @@ import random as r
 lotto=[]#list
 while len(lotto)<5:
     lotto.append(r.randint(1,10))
+    lotto=tuple(lotto)#不可去更改裡面的數值
+
 print(lotto)
-lotto2=set()
+lotto2=set()#set不可重複數組[1,2,3,4,5]
 while len(lotto2)<5:
     lotto2.add(r.randint(1,10))
 print(lotto2)
--- D05/elevator.py
+++ D05/elevator.py
@@ -2,7 +2,7 @@ import time
 print('有一棟7層大樓的電梯系統,運作如下:')
 now=1
 while True:
-    goal=int(input(print('你現在在%d樓,請問要去哪樓?\n>>'%now)))
+    goal=int(input('你現在在%d樓,請問要去哪樓?\n>>'%now))
     if goal<=0 or goal>=8:
         print("尚未有這些樓層,請輸入1~7樓")
         continue
