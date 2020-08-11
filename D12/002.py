7bbecfb3d3d65e554346b7ba423828debb4a9446
stanford <stanford93713@gmail.com>
2020-08-11 20:30:49 +0800

    Python

--- D12/002.py
+++ D12/002.py
@@ -1,16 +1,17 @@
 class BMI:
-    h=0 #公有變數
-    w=0 #公有變數
-    __bmi=0 #私有變數
+    h = 0  # 公有變數
+    w = 0  # 公有變數
+    __bmi = 0  # 私有變數
 
     def calcBMI(self):
-        self.__bmi=self.w/((self.h/100)**2)
+        self.__bmi = self.w / (self.h/100)**2
+
     def getBMI(self):
         return self.__bmi
 
 if __name__ == '__main__':
-    b=BMI
-    b.h=170
-    b.w=75
-    b.calcBMI() #計算BMI
+    b = BMI()
+    b.h = 170
+    b.w = 60
+    b.calcBMI() # 計算 BMI
     print(b.getBMI())
---
+++ D12/006.py
@@ -0,0 +1,31 @@
+class Human:
+    name=''
+    age=0
+    sex=''
+
+    def __init__(self,name,age,sex) -> None:
+        self.name=name
+        self.age=age
+        self.sex=sex
+
+    def __str__(self) -> str:
+        return 'name:%s age:%d sex:%s'%(self.name,self.age,self.sex)
+
+
+class Student(Human):
+    number=0
+    grade=''
+
+    def __init__(self, name, age, sex,number,grade) -> None:
+        super().__init__(name, age, sex)
+        self.number=number
+        self.grade=grade
+
+    def __str__(self) -> str:
+        return super().__str__()+'number:%d grade:%s'%(self.number,self.grade)
+
+
+if __name__=='__main__':
+    st=Student('John',18,'男',1,'一年級')
+    print(st)
+
