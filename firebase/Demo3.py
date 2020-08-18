160890db1c5b5e41d5cbe2f2ca1c997c8a8e82f4
stanford <stanford93713@gmail.com>
2020-08-18 21:58:58 +0800

    python

---
+++ firebase/Demo3.py
@@ -0,0 +1,30 @@
+import tkinter
+from firebase import firebase
+firebase = firebase.FirebaseApplication('https://iotfb-fc0b9.firebaseio.com/', None)
+
+def openTheDoor():
+    result = firebase.patch('/myhouse', {'open': 1})
+
+def closeTheDoor():
+    result = firebase.patch('/myhouse', {'open': 0})
+
+win=tkinter.Tk()
+win.title('Door open')
+win.geometry('300x100')
+
+
+
+
+tkinter.Tk()
+win.title("Door Open")
+win.geometry("300x100")
+
+openButton = tkinter.Button(win, text="Open", command=openTheDoor)
+openButton.config(font=("Arial", 20))
+openButton.pack(side=tkinter.LEFT)
+
+closeButton = tkinter.Button(win, text="Close", command=closeTheDoor)
+closeButton.config(font=("Arial", 20))
+closeButton.pack(side=tkinter.RIGHT)
+
+win.mainloop()
